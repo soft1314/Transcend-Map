c61028faa6eb20333cbd607c4a9966e4ece3063c
soft1314 <13854101314@139.com>
2023-08-05 11:56:30 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<!-- 本地webview -->
+		<web-view src="/static/html/tianditu.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
